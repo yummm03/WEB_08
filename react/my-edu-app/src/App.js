dd1a98e179a2db1764fd31b246adcc4ef76432fc
yummm03 <yumi5610@gmail.com>
2024-09-04 12:54:20 +0900

    0904

--- react/my-edu-app/src/App.js
+++ react/my-edu-app/src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Post from './Post';
+
 
 function App() {
   const element = <h1 className='title'>안녕하세요 h1입니다</h1>
+  const data = [{
+      title : '첫글은 내꺼야',
+      text : '반갑습니다'
+    },{
+      title : '첫번째글인가?',
+      text : '아니었네~'
+    },{
+      title : '등수놀이 유치해',
+      text : '철좀 들어'
+    }]
+
   return (
     <div className="App">
       {element}
 
-      <div>
-        <h3>게시물제목</h3>
-        <p>lorem ipsum 어쩌구저쩌구</p>
-      </div>
+      {data.map((value,key)=>
+        <p>{value.title}</p>
+      )}
+
+      {data.map((value,key)=>
+        <Post 제목={value.title} 내용={value.text}></Post>
+      )}
     </div>
   );
 }
